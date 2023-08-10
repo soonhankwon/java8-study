1acdf9014a282bee1a09ade8439bbc07d92824c7
soonhankwon <soonable@gmail.com>
2023-08-10 09:26:10 +0900

    Initial commit

---
+++ src/main/java/soon/java8study/Java8StudyApplication.java
@@ -0,0 +1,13 @@
+package soon.java8study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java8StudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/soon/java8study/Java8StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package soon.java8study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
